e9f7ece1574f7c13af2438d5eb54ae6e6fb9ac7e
Anmol <anmolsingh1597@gmail.com>
2025-09-20 20:06:12 -0400

    feat: Add testDebugUnitTest dependency to assembleDebug task
    This change ensures that `testDebugUnitTest` runs before `assembleDebug` by adding it as a dependency. This helps in running all tasks in parallel more effectively.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,13 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+    // To run all tasks in parallel
+    tasks.whenTaskAdded {
+        if (name == "assembleDebug"){
+            dependsOn("testDebugUnitTest")
+        }
+    }
 }
 
 dependencies {
